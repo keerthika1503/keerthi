4fb12eaa4c6ca05e751df5177ad9425c8ed136b5
keerthika1503 <Keerthikakeerthi1310@gmail.com>
2023-02-08 12:49:07 +0530

    design

---
+++ src/test/java/org/tset/MainTest.java
@@ -0,0 +1,12 @@
+package org.tset;
+
+public class MainTest {
+	public static void main(String[] args) {
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		
+	}
+
+}
